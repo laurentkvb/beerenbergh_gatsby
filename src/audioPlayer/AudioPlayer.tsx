426878aaccda7b67ef350c80e454cc7994c0bfdb
laurentkvb <lkvb90@gmail.com>
2020-04-19 09:57:39 +0200

    Audioplayer is not enabled on website visit

--- src/audioPlayer/AudioPlayer.tsx
+++ src/audioPlayer/AudioPlayer.tsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 import Sound from "react-sound";
 import { graphql, useStaticQuery } from "gatsby";
-import { detect } from "detect-browser";
 
 export interface AudioPlayerContextProps {
   togglePlayer: Function;
@@ -19,31 +18,9 @@ export const useAudioPlayer = () : AudioPlayerContextProps => React.useContext(A
 
 const AudioPlayer: React.FC = ({ children }) => {
   const [shouldPlay, setShouldPlay] = useState(false);
-  const [isFirstRun, setIsFirstRun] = useState(true);
   const data = useStaticQuery(pageQuery);
   const { audio } = data.contentfulBeerenbergh;
 
-
-  useEffect(() => {
-    setTimeout(() => {
-      if (isFirstRun) {
-        const isChrome = detect()?.name === "chrome";
-        const isSafari = detect()?.name === "safari";
-        const isMobile = detect()?.os === "iOS" || detect()?.os === "Android OS";
-
-        if (isChrome) {
-          setShouldPlay(true);
-          setIsFirstRun(false);
-        } else if (isMobile || isSafari) {
-          setShouldPlay(false);
-          setIsFirstRun(false);
-        } else {
-          setShouldPlay(true);
-          setIsFirstRun(false);
-        }
-      }
-    }, 1000);
-  }, [isFirstRun]);
 
   return (
     <AudioPlayerContext.Provider value={{
